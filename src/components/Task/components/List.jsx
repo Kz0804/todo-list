df0edd57ad721c2963914d70e15e8be9f79d45b0
Kevin Zhu <kaixiang.z1115@gmail.com>
2024-05-19 22:09:45 +1000

    Implement the feature to control item visibility

--- src/components/Task/components/List.jsx
+++ src/components/Task/components/List.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 
 const List = ({ tasks, onComplete }) => {
+  const [showCompleted, setShowCompleted] = useState(true);
+
   return (
     <div className="task--list">
+      <button
+        className="btn-filter"
+        onClick={() => setShowCompleted(!showCompleted)}
+      >
+        {showCompleted ? 'Hide Completed Items' : 'Show Completed Items'}
+      </button>
       <ul>
-        {tasks.map((task) => (
-          <Item key={task.id} task={task} onComplete={onComplete} />
-        ))}
+        {tasks.map(
+          (task) =>
+            (!task.completed || showCompleted) && (
+              <Item key={task.id} task={task} onComplete={onComplete} />
+            )
+        )}
       </ul>
     </div>
   );
